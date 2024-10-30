ab49159c9268a76d204e62bb837de74873d755c5
Baudeur <baudeurbertrand@yahoo.fr>
2024-10-30 13:10:27 +0100

    PATCH add footer

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,25 @@ import { Infos } from "./components/infos/Infos";
 import { People } from "./components/people/People";
 import { Pizzas } from "./components/pizzas/Pizzas";
 import title from "./assets/Title.png";
+import { Footer } from "./components/utils/Footer";
 
 function App() {
   return (
-    <div className="flex flex-col items-center relative">
-      <img src={title} className="w-[800px] my-11" alt="Pizza Party Maker" />
-      <div className="w-full flex justify-end top-32 absolute">
-        <Infos />
-      </div>
-      <div className="flex flex-col items-stretch">
-        <div className="flex">
-          <People />
-          <Pizzas />
+    <div className="flex flex-col justify-between h-full">
+      <div className="flex flex-col items-center relative">
+        <img src={title} className="w-[800px] my-11" alt="Pizza Party Maker" />
+        <div className="w-full flex justify-end top-32 absolute">
+          <Infos />
         </div>
-        <Calculator />
+        <div className="flex flex-col items-stretch">
+          <div className="flex">
+            <People />
+            <Pizzas />
+          </div>
+          <Calculator />
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
